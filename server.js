b249d7411a25183aff85b6015bb25d28bb8b9992
Jan Bölsche <regular.gonzales@googlemail.com>
2015-01-23 12:17:15 +0100

    added package.json, replace console.log with debug, streaming index.html

--- server.js
+++ server.js
@@ -1,23 +1,40 @@
+var debug = require('debug')('santagame');
+
 var http = require('http');
 var fs = require('fs');
 
+var nameList = [
+	'gio',
+	'anna',
+	'chiara',
+	'ella',
+	'sarah',
+	'sara'
+];
+
+debug("hello World. Listening on port 9966");
+
 http.createServer(function (req, res) {
     var contents;
-    console.log(req.url);
-    if (req.url === "/") {
+    debug('someone is requesting %s', req.url);
+    if (req.url === "/list") {
+        debug('we send the list');
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(nameList));
+    } else if (req.url === "/") {
         res.writeHead(200, {'Content-Type': 'text/html'});
-        contents = fs.readFileSync("./index.html", "UTF-8");
-        res.end(contents);
-        console.log("html");
-    }
-    if (req.url === "/script.js") {
+        var stream = fs.createReadStream("./index.html");
+        stream.pipe(res);
+        debug("just sent the html file!");
+    } else if (req.url === "/script.js") {
         res.writeHead(200, {'Content-Type': 'application/javascript'});
         contents = fs.readFileSync("./script.js", "UTF-8");
         res.end(contents);
-        console.log("js");
+        debug("just sent the js file!");
     } else {
-       res.writeHead(404);
-       res.end("oops");
+        debug('file not found!');
+        res.writeHead(404);
+        res.end("oops!");
     }
 
 }).listen(9966);
